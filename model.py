203cf271925046260a99ef434bbf82c0703116df
idanv87 <idanv@campus.technion.ac.il>
2022-07-31 08:55:50 +0300

    w

--- model.py
+++ model.py
@@ -86,7 +86,9 @@ for k in range(Constants.CROSS_VAL):
 
     model = keras.Model(
         inputs=[E1_input, Hx1_input, Hy1_input, E2_input, Hx2_input, Hy2_input, E3_input, Hx3_input, Hy3_input],
-        outputs=[E1_output, Hx1_output, Hy1_output, E2_output, Hx2_output, Hy2_output, E3_output, Hx3_output, Hy3_output]
+        outputs=[E1_output, Hx1_output, Hy1_output, E2_output, Hx2_output, Hy2_output
+            ,E3_output, Hx3_output, Hy3_output
+                 ]
         # outputs = [E_output, Hx_output, Hy_output, energy_output]
     )
 
@@ -94,8 +96,9 @@ for k in range(Constants.CROSS_VAL):
         optimizer=keras.optimizers.Adam(learning_rate=1e-3),
         # loss=[custom_loss, custom_loss, custom_loss],
         loss=[custom_loss, custom_loss, custom_loss,
-              custom_loss, custom_loss,custom_loss,
-              custom_loss, custom_loss,custom_loss]
+              custom_loss, custom_loss,custom_loss
+              ,custom_loss, custom_loss,custom_loss
+              ]
     )
     if k==0:
        model.save(saving_path + 'model.pkl')
@@ -117,7 +120,7 @@ for k in range(Constants.CROSS_VAL):
     # csv loger
     reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.0001)
     history = model.fit(
-        net_input, net_output[:-1],
+        net_input, net_output[:-4],
         callbacks=[earlystopping, model_checkpoint_callback],
         epochs=Constants.EPOCHS,
         batch_size=Constants.BATCH_SIZE,
